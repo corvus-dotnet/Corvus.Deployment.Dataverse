7056271016cab6141f2acf6d4c0cd30143751647
James Dawson <james.dawson@endjin.com>
2024-05-02 21:11:05 +0100

    Add initial build files generated from templates

---
+++ build.ps1
@@ -0,0 +1,96 @@
+[CmdletBinding()]
+param (
+    [Parameter(Position=0)]
+    [string[]] $Tasks = @("."),
+
+    [Parameter()]
+    [string] $SourcesDir = $PWD,
+
+    [Parameter()]
+    [string] $BuildModulePath,
+
+    [Parameter()]
+    [version] $BuildModuleVersion = "1.5.5",
+
+    [Parameter()]
+    [version] $InvokeBuildModuleVersion = "5.10.3"
+)
+
+$ErrorActionPreference = $ErrorActionPreference ? $ErrorActionPreference : 'Stop'
+$InformationPreference = 'Continue'
+
+$here = Split-Path -Parent $PSCommandPath
+
+#region InvokeBuild setup
+if (!(Get-Module -ListAvailable InvokeBuild)) {
+    Install-Module InvokeBuild -RequiredVersion $InvokeBuildModuleVersion -Scope CurrentUser -Force -Repository PSGallery
+}
+Import-Module InvokeBuild
+# This handles calling the build engine when this file is run like a normal PowerShell script
+# (i.e. avoids the need to have another script to setup the InvokeBuild environment and issue the 'Invoke-Build' command )
+if ($MyInvocation.ScriptName -notlike '*Invoke-Build.ps1') {
+    try {
+        Invoke-Build $Tasks $MyInvocation.MyCommand.Path @PSBoundParameters
+    }
+    catch {
+        $_.ScriptStackTrace
+        throw
+    }
+    return
+}
+#endregion
+
+#region Import shared tasks and initialise build framework
+if (!($BuildModulePath)) {
+    if (!(Get-Module -ListAvailable Endjin.RecommendedPractices.Build | ? { $_.Version -eq $BuildModuleVersion })) {
+        Write-Information "Installing 'Endjin.RecommendedPractices.Build' module..."
+        Install-Module Endjin.RecommendedPractices.Build -RequiredVersion $BuildModuleVersion -Scope CurrentUser -Force -Repository PSGallery
+    }
+    $BuildModulePath = "Endjin.RecommendedPractices.Build"
+}
+else {
+    Write-Information "BuildModulePath: $BuildModulePath"
+}
+Import-Module $BuildModulePath -RequiredVersion $BuildModuleVersion -Force
+
+# Load the build process & tasks
+. Endjin.RecommendedPractices.Build.tasks
+#endregion
+
+# Set the required build options
+$PesterTestsDir = "$here/module/functions"
+$PesterShowOptions = @("Describe","Failed","Summary")
+$PowerShellModulesToPublish = @(
+    @{
+        ModulePath = "$here/module/Corvus.Deployment.Dataverse.psd1"
+        FunctionsToExport = @("*")
+        CmdletsToExport = @()
+        AliasesToExport = @()
+    }
+)
+$SkipBuildModuleVersionCheck = $false
+$SkipPrAutoflowVersionCheck = $false
+$SkipPrAutoflowEnrollmentCheck = $true
+
+# Customise the build process
+task . FullBuild
+
+# build extensibility tasks
+task RunFirst {}
+task PreInit {}
+task PostInit {}
+task PreVersion {}
+task PostVersion {}
+task PreBuild {}
+task PostBuild {}
+task PreTest {}
+task PostTest {}
+task PreTestReport {}
+task PostTestReport {}
+task PreAnalysis {}
+task PostAnalysis {}
+task PrePackage {}
+task PostPackage {}
+task PrePublish {}
+task PostPublish {}
+task RunLast {}
